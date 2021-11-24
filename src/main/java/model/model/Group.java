68817ef00f9732d0005cfba0eadd52692d447cd4
magskwa <magali.egger@heig-vd.ch>
2021-11-24 11:07:26 +0100

    classe groupe

--- src/main/java/model/model/Group.java
+++ src/main/java/model/model/Group.java
@@ -1,5 +1,11 @@
 package model.model;
 
+import java.util.ArrayList;
+
 public class Group {
-    private String
+    private ArrayList<Person> members = new ArrayList<Person>();
+
+    public ArrayList<Person> getMembers() {
+        return members;
+    }
 }
