76eca4e1e78173451db27282c1f6801af47b227f
NunezTania <tnunez@bluewin.ch>
2021-11-24 11:21:43 +0100

    ConfigurationManager ok

--- src/main/java/config/config/ConfigurationManager.java
+++ src/main/java/config/config/ConfigurationManager.java
@@ -1,5 +1,6 @@
 package config.config;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -7,7 +8,7 @@ import java.util.Properties;
 public class ConfigurationManager {
     public static void main(String[] args) {
 
-
+        for (String s : new ConfigurationManager().getProperties()) System.out.println(s);
 
     }
 
@@ -16,23 +17,21 @@ public class ConfigurationManager {
     public String[] getProperties() {
 
         String[] propsResult = new String[]{"smtpServerAdress", "smtpServerPort", "numberOfGroups", "witnessesToCC"}; // Champs qu'il y a dans config.properties
-        System.out.println("COUCOU\n");
 
         try {
 
             Properties prop = new Properties();
-            String propFileName = "config.properties";
+            String propFileName = "C:\\Users\\FNGUSER004\\IdeaProjects\\API-2021-SMTP\\src\\main\\java\\config\\config\\config.properties";
 
-            InputStream in = getClass().getClassLoader().getResourceAsStream(propFileName);
-
-            if (in != null) {
-                prop.load(in);
-            }
+            InputStream in = new FileInputStream(propFileName);
+            prop.load(in);
 
             for (int i = 0; i < propsResult.length; i++) {
-                propsResult[i] = new String(prop.getProperty(propsResult[i]));
 
+                propsResult[i] = prop.getProperty(propsResult[i]);
             }
+
+
 
 
         } catch (IOException e) {
