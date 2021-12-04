2f3b3e9b5665787e6fd7bf29bee32f025bf5cdbd
magskwa <magali.egger@heig-vd.ch>
2021-12-04 18:01:34 +0100

    change a lot

--- src/main/java/config/config/ConfigurationManager.java
+++ src/main/java/config/config/ConfigurationManager.java
@@ -1,46 +1,37 @@
 package config.config;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class ConfigurationManager {
-    public static void main(String[] args) {
 
-        for (String s : new ConfigurationManager().getProperties()) System.out.println(s);
-
-    }
-
-    /* Retourne un tableau de String contenant les valeurs du config.properties */
 
     public String[] getProperties() {
-
         String[] propsResult = new String[]{"smtpServerAdress", "smtpServerPort", "numberOfGroups", "witnessesToCC"}; // Champs qu'il y a dans config.properties
 
         try {
-
             Properties prop = new Properties();
             String propFileName = "C:\\Users\\magal\\Documents\\POO\\API-2021-Lab-SMTP\\src\\main\\java\\config<\\config\\config.properties";
-
             InputStream in = new FileInputStream(propFileName);
             prop.load(in);
-
             for (int i = 0; i < propsResult.length; i++) {
-
                 propsResult[i] = prop.getProperty(propsResult[i]);
             }
-
-
-
-
         } catch (IOException e) {
-
             System.err.println(e.getMessage());
-
         }
-
         return propsResult;
+    }
 
+    public ArrayList<String> getVictims(){
+        try {
+            FileInputStream fi = new FileInputStream(".\\config\\config\\victims.utf8");
+            BufferedReader in = new BufferedReader(new InputStreamReader(fi));
+            ArrayList<String> victims = new ArrayList<>();
+            while (in.readLine() != -1){
+                victims.add(in.);
+            }
+        }
     }
 }
--- src/main/java/model/model/PrankGenerator.java
+++ src/main/java/model/model/PrankGenerator.java
@@ -1,4 +1,35 @@
 package model.model;
 
+import config.config.ConfigurationManager;
+import smtp.SMTPclient;
+
 public class PrankGenerator {
+
+    private ConfigurationManager manager;
+
+
+    public Prank generatePranks() {
+        String[] properties = manager.getProperties();
+
+        // TODO
+        // partie 1 : va chercher les info dans les config
+        // 1. la liste d'email victime
+        // 2. les messages
+        Person victim = new Person("magali.egger@heig-vd.ch");
+        Person sender = new Person("magali.egger@heig-vd.ch");
+        String message = "Hello\n Blabla bla. Voila \n Bye";
+
+        Prank p = new Prank(victim, sender, message);
+        // liste de prank a crée et a envoyé a smtp
+
+        return p;
+    }
+
 }
+
+
+
+
+
+
+
